410bad94aad14f1aa0fbbe41f823cd13963e42c0
Qiming Chu <cchuqiming@gmail.com>
2023-03-29 21:50:18 +0800

    Add config files

---
+++ env.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> {}; [
+  calc
+  coreutils-full
+  fd
+  gccgo
+  gdb
+  jq
+  nasm
+  neovim
+  nodejs
+  pwgen
+  python3
+  ripgrep
+  ruby_3_1
+  llvm
+  shellcheck
+  socat
+  tmux
+  tree
+  universal-ctags
+  zsh
+  zsh-git-prompt
+  zsh-system-clipboard
+  zsh-z
+  zsh-syntax-highlighting
+]
+
