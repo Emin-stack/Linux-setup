043aa3fffb89f5c08cf5f19f4af1c43ca7696b3f
Qiming Chu <cchuqiming@gmail.com>
2024-01-22 21:30:06 +0800

    scripts: Add scripts for docker

--- utils/docker/create.sh
+++ utils/docker/create.sh
@@ -2,6 +2,14 @@
 #
 # docker pull ubuntu
 # docker run -it -p 5901:5901 -p 6789:22 --gpus all -v /mnt:/mnt --name ubuntu ubuntu
+#
+# docker run -it -p 5903:5901 -p 6789:22 -p 7777:8080 -p 1234:1234 --name myubuntu --gpus all -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix \
+#  -e DISPLAY=unix$DISPLAY \
+#  -e GDK_SCALE \
+#  -e GDK_DPI_SCALE \
+#  ${tag_name}
+
+
 
 apt-get update
 apt-get -y install xorg openbox locales xfce4 xfce4-goodies xfce4-terminal vim git wget net-tools tightvncserver
